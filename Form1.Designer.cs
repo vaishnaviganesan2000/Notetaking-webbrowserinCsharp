89f7db70196140954e449eeda4f47208a888a43a
vaishnaviganesan2000 <124806117+vaishnaviganesan2000@users.noreply.github.com>
2023-02-12 15:32:47 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace Reproj
+{
+    partial class Notetaker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notebox = new System.Windows.Forms.TextBox();
+            this.titlebox = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.NewNoteButton = new System.Windows.Forms.Button();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.clocklabel = new System.Windows.Forms.Label();
+            this.clocktimer = new System.Windows.Forms.Timer(this.components);
+            this.redButton = new System.Windows.Forms.Button();
+            this.greenButton = new System.Windows.Forms.Button();
+            this.blueButton = new System.Windows.Forms.Button();
+            this.styleButton1 = new System.Windows.Forms.Button();
+            this.styleButton2 = new System.Windows.Forms.Button();
+            this.clocklabel2 = new System.Windows.Forms.Label();
+            this.blackButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.previousNotes = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.previousNotes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notebox
+            // 
+            this.notebox.Location = new System.Drawing.Point(526, 159);
+            this.notebox.Multiline = true;
+            this.notebox.Name = "notebox";
+            this.notebox.Size = new System.Drawing.Size(438, 349);
+            this.notebox.TabIndex = 0;
+            // 
+            // titlebox
+            // 
+            this.titlebox.Location = new System.Drawing.Point(526, 106);
+            this.titlebox.Name = "titlebox";
+            this.titlebox.Size = new System.Drawing.Size(438, 22);
+            this.titlebox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(527, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(437, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "TITLE:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(523, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(441, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "NOTE:";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.BackColor = System.Drawing.Color.White;
+            this.SaveButton.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SaveButton.Location = new System.Drawing.Point(16, 514);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(223, 27);
+            this.SaveButton.TabIndex = 5;
+            this.SaveButton.Text = "SAVE";
+            this.SaveButton.UseVisualStyleBackColor = false;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.BackColor = System.Drawing.Color.White;
+            this.DeleteButton.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteButton.Location = new System.Drawing.Point(259, 514);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(239, 27);
+            this.DeleteButton.TabIndex = 6;
+            this.DeleteButton.Text = "DELETE";
+            this.DeleteButton.UseVisualStyleBackColor = false;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // NewNoteButton
+            // 
+            this.NewNoteButton.BackColor = System.Drawing.Color.White;
+            this.NewNoteButton.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NewNoteButton.Location = new System.Drawing.Point(16, 555);
+            this.NewNoteButton.Name = "NewNoteButton";
+            this.NewNoteButton.Size = new System.Drawing.Size(227, 31);
+            this.NewNoteButton.TabIndex = 7;
+            this.NewNoteButton.Text = "NEW NOTE";
+            this.NewNoteButton.UseVisualStyleBackColor = false;
+            this.NewNoteButton.Click += new System.EventHandler(this.NewNoteButton_Click);
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.BackColor = System.Drawing.Color.White;
+            this.LoadButton.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoadButton.Location = new System.Drawing.Point(259, 555);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(239, 31);
+            this.LoadButton.TabIndex = 8;
+            this.LoadButton.Text = "LOAD";
+            this.LoadButton.UseVisualStyleBackColor = false;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // clocklabel
+            // 
+            this.clocklabel.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clocklabel.Location = new System.Drawing.Point(844, 601);
+            this.clocklabel.Name = "clocklabel";
+            this.clocklabel.Size = new System.Drawing.Size(120, 22);
+            this.clocklabel.TabIndex = 10;
+            this.clocklabel.Text = "TIME:";
+            this.clocklabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.clocklabel.Click += new System.EventHandler(this.clocklabel_Click);
+            // 
+            // clocktimer
+            // 
+            this.clocktimer.Interval = 1000;
+            this.clocktimer.Tick += new System.EventHandler(this.clocktimer_Tick);
+            // 
+            // redButton
+            // 
+            this.redButton.BackColor = System.Drawing.Color.Red;
+            this.redButton.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.redButton.ForeColor = System.Drawing.Color.Transparent;
+            this.redButton.Location = new System.Drawing.Point(635, 555);
+            this.redButton.Name = "redButton";
+            this.redButton.Size = new System.Drawing.Size(89, 31);
+            this.redButton.TabIndex = 11;
+            this.redButton.Text = "RED";
+            this.redButton.UseVisualStyleBackColor = false;
+            this.redButton.Click += new System.EventHandler(this.redButton_Click);
+            // 
+            // greenButton
+            // 
+            this.greenButton.BackColor = System.Drawing.Color.Lime;
+            this.greenButton.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.greenButton.ForeColor = System.Drawing.Color.Transparent;
+            this.greenButton.Location = new System.Drawing.Point(758, 555);
+            this.greenButton.Name = "greenButton";
+            this.greenButton.Size = new System.Drawing.Size(95, 31);
+            this.greenButton.TabIndex = 12;
+            this.greenButton.Text = "GREEN";
+            this.greenButton.UseVisualStyleBackColor = false;
+            this.greenButton.Click += new System.EventHandler(this.greenButton_Click);
+            // 
+            // blueButton
+            // 
+            this.blueButton.BackColor = System.Drawing.Color.Blue;
+            this.blueButton.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blueButton.ForeColor = System.Drawing.Color.Transparent;
+            this.blueButton.Location = new System.Drawing.Point(867, 555);
+            this.blueButton.Name = "blueButton";
+            this.blueButton.Size = new System.Drawing.Size(97, 31);
+            this.blueButton.TabIndex = 13;
+            this.blueButton.Text = "BLUE";
+            this.blueButton.UseVisualStyleBackColor = false;
+            this.blueButton.Click += new System.EventHandler(this.blueButton_Click);
+            // 
+            // styleButton1
+            // 
+            this.styleButton1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.styleButton1.Location = new System.Drawing.Point(526, 514);
+            this.styleButton1.Name = "styleButton1";
+            this.styleButton1.Size = new System.Drawing.Size(198, 27);
+            this.styleButton1.TabIndex = 14;
+            this.styleButton1.Text = "STYLE 1";
+            this.styleButton1.UseVisualStyleBackColor = true;
+            this.styleButton1.Click += new System.EventHandler(this.styleButton1_Click);
+            // 
+            // styleButton2
+            // 
+            this.styleButton2.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.styleButton2.Location = new System.Drawing.Point(758, 514);
+            this.styleButton2.Name = "styleButton2";
+            this.styleButton2.Size = new System.Drawing.Size(210, 27);
+            this.styleButton2.TabIndex = 15;
+            this.styleButton2.Text = "STYLE 2";
+            this.styleButton2.UseVisualStyleBackColor = true;
+            this.styleButton2.Click += new System.EventHandler(this.styleButton2_Click);
+            // 
+            // clocklabel2
+            // 
+            this.clocklabel2.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clocklabel2.Location = new System.Drawing.Point(12, 601);
+            this.clocklabel2.Name = "clocklabel2";
+            this.clocklabel2.Size = new System.Drawing.Size(113, 22);
+            this.clocklabel2.TabIndex = 16;
+            this.clocklabel2.Text = "DATE:";
+            this.clocklabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // blackButton
+            // 
+            this.blackButton.BackColor = System.Drawing.Color.Black;
+            this.blackButton.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blackButton.ForeColor = System.Drawing.Color.Transparent;
+            this.blackButton.Location = new System.Drawing.Point(527, 555);
+            this.blackButton.Name = "blackButton";
+            this.blackButton.Size = new System.Drawing.Size(93, 31);
+            this.blackButton.TabIndex = 17;
+            this.blackButton.Text = "BLACK";
+            this.blackButton.UseVisualStyleBackColor = false;
+            this.blackButton.Click += new System.EventHandler(this.blackButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(490, 25);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "PREVIOUS NOTE:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(867, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 31);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(437, 23);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "WEB BROWSER:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(5, 36);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(856, 31);
+            this.textBox2.TabIndex = 23;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // previousNotes
+            // 
+            this.previousNotes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.previousNotes.BackgroundColor = System.Drawing.Color.White;
+            this.previousNotes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.previousNotes.Location = new System.Drawing.Point(16, 106);
+            this.previousNotes.Name = "previousNotes";
+            this.previousNotes.RowHeadersWidth = 51;
+            this.previousNotes.RowTemplate.Height = 24;
+            this.previousNotes.Size = new System.Drawing.Size(501, 402);
+            this.previousNotes.TabIndex = 24;
+            // 
+            // Notetaker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(976, 632);
+            this.Controls.Add(this.previousNotes);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.blackButton);
+            this.Controls.Add(this.clocklabel2);
+            this.Controls.Add(this.styleButton2);
+            this.Controls.Add(this.styleButton1);
+            this.Controls.Add(this.blueButton);
+            this.Controls.Add(this.greenButton);
+            this.Controls.Add(this.redButton);
+            this.Controls.Add(this.clocklabel);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.NewNoteButton);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.titlebox);
+            this.Controls.Add(this.notebox);
+            this.Name = "Notetaker";
+            this.Text = "NoteTaker";
+            this.Load += new System.EventHandler(this.Notetaker_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.previousNotes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox notebox;
+        private System.Windows.Forms.MaskedTextBox titlebox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button NewNoteButton;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Label clocklabel;
+        private System.Windows.Forms.Timer clocktimer;
+        private System.Windows.Forms.Button redButton;
+        private System.Windows.Forms.Button greenButton;
+        private System.Windows.Forms.Button blueButton;
+        private System.Windows.Forms.Button styleButton1;
+        private System.Windows.Forms.Button styleButton2;
+        private System.Windows.Forms.Label clocklabel2;
+        private System.Windows.Forms.Button blackButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView previousNotes;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Reproj
+{
+    public partial class Notetaker : Form
+    {
+        
+        DataTable notes = new DataTable();
+        bool editing = false;
+        public Notetaker()
+        {
+            InitializeComponent();
+        }
+        private void Notetaker_Load(object sender, EventArgs e)
+        {
+            notes.Columns.Add("Title");
+            notes.Columns.Add("Note");
+
+            previousNotes.DataSource = notes;
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+           if(editing)
+            {
+                notes.Rows[previousNotes.CurrentCell.RowIndex]["Title"] = titlebox.Text;
+                notes.Rows[previousNotes.CurrentCell.RowIndex]["Note"] = notebox.Text;
+
+            }
+            else
+            {
+                notes.Rows.Add(titlebox.Text,notebox.Text);
+            }
+            titlebox.Text = "";
+            notebox.Text = "";
+            editing = false;
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                notes.Rows[previousNotes.CurrentCell.RowIndex].Delete();
+            }
+            catch
+            {
+                Exception ex = new Exception();
+                Console.WriteLine("Not A Valid Note");
+            }
+        }
+
+        private void NewNoteButton_Click(object sender, EventArgs e)
+        {
+            titlebox.Text = "";
+            notebox.Text = "";
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            titlebox.Text = notes.Rows[previousNotes.CurrentCell.RowIndex].ItemArray[0].ToString();
+            notebox.Text = notes.Rows[previousNotes.CurrentCell.RowIndex].ItemArray[1].ToString();
+            editing= true;
+        }
+
+        private void previousNotes_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            titlebox.Text = notes.Rows[previousNotes.CurrentCell.RowIndex].ItemArray[0].ToString();
+            notebox.Text = notes.Rows[previousNotes.CurrentCell.RowIndex].ItemArray[1].ToString();
+            editing = true;
+        }
+
+        private void clocktimer_Tick(object sender, EventArgs e)
+        {
+            clocklabel2.Text = DateTime.Now.ToString("dddd,dd MMMM yyyy");
+            clocklabel.Text = DateTime.Now.ToString("hh:mm:ss");
+        }
+
+        private void clocklabel_Click(object sender, EventArgs e)
+        {
+            clocktimer.Start();
+        }
+
+        private void redButton_Click(object sender, EventArgs e)
+        {
+            titlebox.ForeColor = Color.Red;
+            notebox.ForeColor = Color.Red;
+           
+
+
+
+        }
+
+        private void greenButton_Click(object sender, EventArgs e)
+        {
+            titlebox.ForeColor = Color.Green;
+            notebox.ForeColor = Color.Green;
+            
+
+        }
+
+        private void blueButton_Click(object sender, EventArgs e)
+        {
+            titlebox.ForeColor = Color.Blue;
+            notebox.ForeColor = Color.Blue;
+           
+
+        }
+
+        private void styleButton1_Click(object sender, EventArgs e)
+        {
+            titlebox.Font = new Font("Times New Roman", 9, FontStyle.Regular);
+            notebox.Font = new Font("Times New Roman", 9, FontStyle.Regular);
+
+        }
+
+        private void styleButton2_Click(object sender, EventArgs e)
+        {
+            titlebox.Font = new Font("Tahoma", 9, FontStyle.Bold);
+            notebox.Font = new Font("Tahoma", 9, FontStyle.Bold);
+           
+        }
+
+        private void blackButton_Click(object sender, EventArgs e)
+        {
+            titlebox.ForeColor = Color.Black;
+            notebox.ForeColor = Color.Black;
+           
+
+        }
+
+        public static void Search(string Query)
+        {
+            Process.Start("https://www.bing.com/search?q=" + Query);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Search(textBox2.Text);
+        }
+
+        
+    }
+}
